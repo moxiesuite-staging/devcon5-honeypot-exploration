9c381e05ef4b44f894cff435b5ec3e4cd644ca9f
David Murdoch <david@davidmurdoch.com>
2019-10-04 22:32:50 -0400

    rename truffle config to new standard

---
+++ truffle.js
@@ -0,0 +1,22 @@
+module.exports = {
+  networks: {
+    local: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    },
+    forked: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    }
+  },
+  compilers: {
+    solc: {
+      version: "^0.5.8",
+      settings: {
+        evmVersion: "byzantium"
+      }
+    }
+  }
+};
